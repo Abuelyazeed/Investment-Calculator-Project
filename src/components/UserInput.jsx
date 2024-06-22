52fb32ace84b26e9092bcd38408c240f157c96d6
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-22 10:15:10 +0300

    created the user input component

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function UserInput() {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="initialInvestment">Initial Investment</label>
+          <input type="number" id="initialInvestment" required />
+        </p>
+        <p>
+          <label htmlFor="annualInvestment">Annual Investment</label>
+          <input type="number" id="annualInvestment" required />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label htmlFor="expectedReturn">Expected Return</label>
+          <input type="number" id="expectedReturn" required />
+        </p>
+
+        <p>
+          <label htmlFor="duration">Duration</label>
+          <input type="number" id="duration" required />
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default UserInput;
