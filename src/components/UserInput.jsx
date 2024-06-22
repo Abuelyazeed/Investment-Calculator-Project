35c116843df42e214cdc0680e0d9402458880457
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-22 10:23:14 +0300

    created userInput state

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,28 +1,63 @@
-import React from "react";
+import { useState } from "react";
 
 function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 15000,
+    annualInvestment: 900,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleOnChange(e) {
+    const { id, value } = e.target;
+    setUserInput({ ...userInput, [id]: value });
+  }
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label htmlFor="initialInvestment">Initial Investment</label>
-          <input type="number" id="initialInvestment" required />
+          <input
+            type="number"
+            id="initialInvestment"
+            value={userInput.initialInvestment}
+            onChange={handleOnChange}
+            required
+          />
         </p>
         <p>
           <label htmlFor="annualInvestment">Annual Investment</label>
-          <input type="number" id="annualInvestment" required />
+          <input
+            type="number"
+            id="annualInvestment"
+            value={userInput.annualInvestment}
+            onChange={handleOnChange}
+            required
+          />
         </p>
       </div>
 
       <div className="input-group">
         <p>
           <label htmlFor="expectedReturn">Expected Return</label>
-          <input type="number" id="expectedReturn" required />
+          <input
+            type="number"
+            id="expectedReturn"
+            value={userInput.expectedReturn}
+            onChange={handleOnChange}
+            required
+          />
         </p>
 
         <p>
           <label htmlFor="duration">Duration</label>
-          <input type="number" id="duration" required />
+          <input
+            type="number"
+            id="duration"
+            value={userInput.duration}
+            onChange={handleOnChange}
+            required
+          />
         </p>
       </div>
     </section>
