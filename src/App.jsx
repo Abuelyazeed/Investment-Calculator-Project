bc40929dbb0ac0f76a50d7d194358abe0a53c8ca
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-22 09:55:04 +0300

    Initialize

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        <img src="investment-calculator-logo.png" alt="A bag with cash in it" />
+        <h1>Investment Calculator</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
